d0f4a3a273174c43d6a18ec49c0d82914d24bf26
zocleep <zocleep@gmail.com>
2022-04-06 17:55:30 +0300

    decorative changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,6 @@ public class Main {
         final String TOKEN = env.get("BOT_TOKEN");
         final String BOT_USERNAME = env.get("BOT_USERNAME");
         final String FATHER_ID = env.get("FATHER_ID");
-        System.out.println("INIT=================");
-        for(String someenv : env.keySet()) {
-            System.out.format("%s=%s%n", someenv, env.get(someenv));
-        }
-        System.out.println("INIT=================");
         Bot bot = new Bot(TOKEN, BOT_USERNAME);
         bot.connect();
         System.out.println("Father ID: " + FATHER_ID);
